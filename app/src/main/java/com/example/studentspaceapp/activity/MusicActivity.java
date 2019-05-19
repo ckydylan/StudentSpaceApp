f522771c926b833ee573cdf7079d142998199f97
dylan <1205428733@qq.com>
2019-05-19 21:44:53 +0800

    #date 5.19 更新音乐

---
+++ app/src/main/java/com/example/studentspaceapp/activity/MusicActivity.java
@@ -0,0 +1,72 @@
+package com.example.studentspaceapp.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import com.example.studentspaceapp.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MusicActivity extends Activity {
+    Map<String, Object> map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music);
+
+        ListView lv_show = findViewById(R.id.lv_show);
+        SimpleAdapter simpleAdapter = new SimpleAdapter(this
+                , getData()
+                , R.layout.activity_music
+                , new String[]{"image", "title", "author","rightImg"}
+                , new int[]{R.id.my_images, R.id.tv_titles, R.id.tv_authors,R.id.img_right});
+        lv_show.setAdapter(simpleAdapter);
+
+    }
+
+    private ArrayList<Map<String, Object>> getData() {
+        ArrayList<Map<String, Object>> list = new ArrayList<>();
+        map = new HashMap<>();
+        map.put("image", R.drawable.music_img1);
+        map.put("title","陪我长大");
+        map.put("author","火箭少女101段奥娟");
+        map.put("rightImg",R.drawable.music_img_right);
+        list.add(map);
+
+        map = new HashMap<>();
+        map.put("image",R.drawable.music_img2);
+        map.put("title","身骑白马");
+        map.put("author","徐佳莹");
+        map.put("rightImg",R.drawable.music_img_right);
+        list.add(map);
+
+        map = new HashMap<>();
+        map.put("image",R.drawable.music_img3);
+        map.put("title","倒数");
+        map.put("author","G.E.M.邓紫棋");
+        map.put("rightImg",R.drawable.music_img_right);
+        list.add(map);
+
+        map = new HashMap<>();
+        map.put("image",R.drawable.music_img4);
+        map.put("title","某某");
+        map.put("author","孟凡明");
+        map.put("rightImg",R.drawable.music_img_right);
+        list.add(map);
+
+        map = new HashMap<>();
+        map.put("image",R.drawable.music_img5);
+        map.put("title","爱殇");
+        map.put("author","小时/龚骏");
+        map.put("rightImg",R.drawable.music_img_right);
+        list.add(map);
+
+
+        return list;
+    }
+}
--- app/src/main/java/com/example/studentspaceapp/fargment/ToolsFragment.java
+++ app/src/main/java/com/example/studentspaceapp/fargment/ToolsFragment.java
@@ -14,10 +14,12 @@ import androidx.fragment.app.Fragment;
 import com.example.studentspaceapp.R;
 import com.example.studentspaceapp.activity.CalculatorActivity;
 import com.example.studentspaceapp.activity.ClassManageActivity;
+import com.example.studentspaceapp.activity.MusicActivity;
 
 public class ToolsFragment extends Fragment {
     CardView cv_into_tool;
     CardView cv_into_class_manage;
+    CardView cv_into_music;
 
     @Nullable
     @Override
@@ -25,6 +27,7 @@ public class ToolsFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_tools,container,false);
         cv_into_tool = view.findViewById(R.id.cv_into_tool);
         cv_into_class_manage = view.findViewById(R.id.cv_into_class_manage);
+        cv_into_music = view.findViewById(R.id.cv_into_music);
 
         cv_into_tool.setOnClickListener(v->{
             Intent intent = new Intent(getActivity(), CalculatorActivity.class);
@@ -33,6 +36,11 @@ public class ToolsFragment extends Fragment {
 
         cv_into_class_manage.setOnClickListener(v->{
             Intent intent = new Intent(getActivity(), ClassManageActivity.class);
+            startActivity(intent);
+        });
+
+        cv_into_music.setOnClickListener(v->{
+            Intent intent = new Intent(getActivity(), MusicActivity.class);
             startActivity(intent);
         });
 
